e3988f8b57175eed8e47e4c70f661443cba5a6b8
Meijer,L. (Lucas) <l.meijer6@students.uu.nl>
2025-04-21 19:59:44 +0200

    Initial setup Battle Pokedex

--- config/formats.ts
+++ config/formats.ts
@@ -28,7 +28,7 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 	{
 		name: "[Gen 9] Random Battle",
 		desc: `Randomized teams of Pok&eacute;mon with sets that are generated to be competitively viable.`,
-		mod: 'gen9',
+		mod: 'pokedex',
 		team: 'random',
 		ruleset: ['PotD', 'Obtainable', 'Species Clause', 'HP Percentage Mod', 'Cancel Mod', 'Sleep Clause Mod', 'Illusion Level Mod'],
 	},
